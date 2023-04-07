e080bdd498eacb919199f660a911af9086b3ca68
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-07 22:37:28 +0200

    Added a Model for Authors

---
+++ WpfApp/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WpfApp.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       public string Description { get; set; }
+
+       public virtual ICollection<Book> Books 
+       { get; private set; } = new ObservableCollection<Book>();
+    }
+}
