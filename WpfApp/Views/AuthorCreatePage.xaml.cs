9dab886edbb28256d3ac0f7d44df6c51aa55d0ea
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-08 11:23:57 +0200

    Added a Page for creating Authors

---
+++ WpfApp/Views/AuthorCreatePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp.Views
+{
+    public partial class AuthorCreatePage : Page
+    {
+        public AuthorCreatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
