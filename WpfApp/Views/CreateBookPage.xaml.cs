2bbeeb30db01cc47ff4e605e57813a57fda8d4e3
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-09 14:48:08 +0200

    + Renamed the file and Added a Combox box for selecting Genres

---
+++ WpfApp/Views/CreateBookPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using WpfApp.ViewModels;
+
+namespace WpfApp.Views
+{
+    public partial class CreateBookPage : Page
+    {
+        public CreateBookPage()
+        {
+            InitializeComponent();
+            DataContext = new CreateBookViewModel();
+        }
+    }
+}
