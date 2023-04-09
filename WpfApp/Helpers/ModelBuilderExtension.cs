9cea36a137cdf2a1097658e572f2ea6cb0276742
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-09 14:51:37 +0200

    * Updated ModelBuilderExtension.cs with more data

--- WpfApp/Helpers/ModelBuilderExtension.cs
+++ WpfApp/Helpers/ModelBuilderExtension.cs
@@ -12,27 +12,27 @@ namespace WpfApp.Helpers
         
             var authors = new List<Author>
             {
-                new Author { Id = 1, Name = "J.K. Rowling", Description = "British author" },
-                new Author { Id = 2,Name = "J.R.R. Tolkien", Description = "British author" },
-                new Author { Id = 3,Name = "George R.R. Martin", Description = "American author" },
-                new Author { Id = 4, Name = "Stephen King", Description = "American author" },
-                new Author { Id = 5, Name = "Agatha Christie", Description = "British author" },
-                new Author { Id = 6, Name = "Jane Austen", Description = "English novelist" },
-                new Author { Id = 7, Name = "Leo Tolstoy", Description = "Russian writer" },
-                new Author { Id = 8, Name = "Fyodor Dostoevsky", Description = "Russian novelist" }
+                new() { Id = 1, Name = "J.K. Rowling", Description = "British author" },
+                new() { Id = 2,Name = "J.R.R. Tolkien", Description = "British author" },
+                new() { Id = 3,Name = "George R.R. Martin", Description = "American author" },
+                new() { Id = 4, Name = "Stephen King", Description = "American author" },
+                new() { Id = 5, Name = "Agatha Christie", Description = "British author" },
+                new() { Id = 6, Name = "Jane Austen", Description = "English novelist" },
+                new() { Id = 7, Name = "Leo Tolstoy", Description = "Russian writer" },
+                new() { Id = 8, Name = "Fyodor Dostoevsky", Description = "Russian novelist" }
             };
 
             var books = new List<Book>
             {
-                new Book { Id = 1,Name = "Harry Potter and the Philosopher's Stone", Description = "First book in the Harry Potter series", Genre = Book.BookType.Fantasy, AuthorId = 1 },
-                new Book { Id = 2, Name = "The Lord of the Rings", Description = "Epic high-fantasy novel", Genre = Book.BookType.Fantasy, AuthorId = 2 },
-                new Book { Id = 3, Name = "A Game of Thrones", Description = "First book in the A Song of Ice and Fire series", Genre = Book.BookType.Fantasy, AuthorId = 3 },
-                new Book { Id = 4, Name = "The Shining", Description = "Horror novel", Genre = Book.BookType.Horror, AuthorId = 4 },
-                new Book { Id = 5, Name = "Murder on the Orient Express", Description = "Detective novel", Genre = Book.BookType.Mystery, AuthorId = 5 },
-                new Book { Id = 6, Name = "Pride and Prejudice", Description = "Novel of manners", Genre = Book.BookType.Romance, AuthorId = 6 },
-                new Book { Id = 7, Name = "War and Peace", Description = "Historical novel", Genre = Book.BookType.Classics, AuthorId = 7 },
-                new Book { Id = 8, Name = "Crime and Punishment", Description = "Psychological thriller", Genre = Book.BookType.Crime, AuthorId = 8 },
-                new Book { Id = 9, Name = "Harry Potter and the Chambers of Secrets", Description = "Second book in the Harry Potter series", Genre = Book.BookType.Fantasy, AuthorId = 1}
+                new() { Id = 1,Name = "Harry Potter and the Philosopher's Stone", Description = "First book in the Harry Potter series", Genre = BookType.Fantasy, AuthorId = 1 },
+                new() { Id = 2, Name = "The Lord of the Rings", Description = "Epic high-fantasy novel", Genre = BookType.Fantasy, AuthorId = 2 },
+                new() { Id = 3, Name = "A Game of Thrones", Description = "First book in the A Song of Ice and Fire series", Genre = BookType.Fantasy, AuthorId = 3 },
+                new() { Id = 4, Name = "The Shining", Description = "Horror novel", Genre = BookType.Horror, AuthorId = 4 },
+                new() { Id = 5, Name = "Murder on the Orient Express", Description = "Detective novel", Genre = BookType.Mystery, AuthorId = 5 },
+                new() { Id = 6, Name = "Pride and Prejudice", Description = "Novel of manners", Genre = BookType.Romance, AuthorId = 6 },
+                new() { Id = 7, Name = "War and Peace", Description = "Historical novel", Genre = BookType.Classics, AuthorId = 7 },
+                new() { Id = 8, Name = "Crime and Punishment", Description = "Psychological thriller", Genre = BookType.Crime, AuthorId = 8 },
+                new() { Id = 9, Name = "Harry Potter and the Chambers of Secrets", Description = "Second book in the Harry Potter series", Genre = BookType.Fantasy, AuthorId = 1}
                 
             };
             modelBuilder.Entity<Author>().HasData(authors);
