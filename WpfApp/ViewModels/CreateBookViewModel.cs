dd8ba6f0de7c1f2d04b62f870349f35c75ed83a4
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-09 23:53:29 +0200

    + Implemented inserting data into the database

--- WpfApp/ViewModels/CreateBookViewModel.cs
+++ WpfApp/ViewModels/CreateBookViewModel.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Security;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
 using WpfApp.Helpers;
 using WpfApp.Models;
 
@@ -7,8 +14,13 @@ namespace WpfApp.ViewModels
 {
     public class CreateBookViewModel : ObservableObject
     {
+        private readonly DataContext _context = new DataContext();
+        
         public string NameProperty { get; set; }
         public string DescriptionProperty { get; set; }
+        public string TestValue { get; set; }
+        
+        public int Id {get; set; }
         private BookType[] _genres = Enum.GetValues(typeof(BookType)).Cast<BookType>().ToArray();
         public BookType[] Genres
         {
@@ -27,22 +39,83 @@ namespace WpfApp.ViewModels
                 OnPropertyChanged();
             }
         }
-        private string _pageName { get; set; }
 
-        public string PageName
+        private ObservableCollection<Author> _authors;
+
+        public ObservableCollection<Author> Authors
         {
-            get => _pageName;
+            get => _authors;
             set
             {
-                if (_pageName == value) return;
-                _pageName = value;
+                _authors = value;
                 OnPropertyChanged();
             }
         }
 
+        public ICommand CreateBookCommand { get; set; }
+        
         public CreateBookViewModel()
         {
-            PageName = "Test";
+            _context.Database.EnsureCreated();
+            _context.Authors.Load();
+            InitializeCommands();
+            
+            Authors = _context.Authors.Local.ToObservableCollection();
+        }
+
+        public void InitializeCommands()
+        {
+            CreateBookCommand = new RelayCommand(CreateAuthor);
+        }
+        private string _searchQuery;
+
+        public string SearchQuery
+        {
+            get => _searchQuery;
+            set
+            {
+                if (_searchQuery == value) return;
+                _searchQuery = value;
+                OnPropertyChanged();
+                SearchAuthors(_searchQuery);
+            }
+        }
+
+        public void SearchAuthors(string searchQuery)
+        {
+            Debug.WriteLine($"SearchAuthor: {searchQuery}");
+            if (string.IsNullOrEmpty(searchQuery))
+            {
+                Authors = _context.Authors.Local.ToObservableCollection();
+            }
+            else
+            {
+                var matchingAuthors = _context.Authors
+                    .Where(a => a.Name.Contains(searchQuery))
+                    .ToList();
+                Authors = new ObservableCollection<Author>(matchingAuthors);
+            }
+        }
+
+        public void CreateAuthor(object e)
+        {
+            try
+            {
+                _context.Books.Add(new()
+                {
+                    Name = NameProperty,
+                    Description = DescriptionProperty,
+                    AuthorId = Id,
+                    Genre = SelectedGenre
+                });
+                _context.SaveChanges();
+                Trace.WriteLine("Success");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
         }
     }
 }
