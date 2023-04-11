3689f27a3bf370f520336e59c861d546de0bff16
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-11 23:32:21 +0200

    + Added the code for inserting Authors into the database

---
+++ WpfApp/Views/CreateAuthorPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using WpfApp.Helpers;
+using WpfApp.Models;
+using WpfApp.ViewModels;
+
+namespace WpfApp.Views
+{
+    public partial class CreateAuthorPage : Page
+    {
+        public CreateAuthorPage()
+        {
+            InitializeComponent();
+            DataContext = new CreateAuthorViewModel();
+        }
+
+    }
+}
