e9166e687f8f11f4cfbada6d7dbe913d1bd4addf
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-13 22:53:16 +0200

    + Added logic for saving changes when you click on the button and working on styling the page

--- WpfApp/ViewModels/AuthorBooksViewModel.cs
+++ WpfApp/ViewModels/AuthorBooksViewModel.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using WpfApp.Helpers;
 using WpfApp.Models;
-using System.Linq;
+using System.Windows.Input;
 
 namespace WpfApp.ViewModels
 {
     public class AuthorBooksViewModel : ObservableObject
     {
-        private DataContext _context = new DataContext();
+        private DataContext _context = new();
         private Author _selectedAuthor { get; set; }
-        
+
         public Author SelectedAuthor
         {
             get => _selectedAuthor;
@@ -21,17 +24,40 @@ namespace WpfApp.ViewModels
                 OnPropertyChanged(nameof(AuthorBooks));
             }
         }
-        public ObservableCollection<Author> AuthorList { get; set; }
+
+        private ObservableCollection<Author> _authorList { get; set; }
+
+        public ObservableCollection<Author> AuthorList
+        {
+            get => _authorList;
+            set
+            {
+                _authorList = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private BookType[] _genreList = Enum.GetValues(typeof(BookType)).Cast<BookType>().ToArray();
+
+        public BookType[] GenreList => _genreList;
+
+        public ICommand CellEditEndingCommand { get; set; }
 
         public ObservableCollection<Book> AuthorBooks =>
-                SelectedAuthor != null ? new ObservableCollection<Book>(SelectedAuthor?.Books) : null;
+            SelectedAuthor != null ? new ObservableCollection<Book>(SelectedAuthor?.Books) : null;
 
 
         public AuthorBooksViewModel()
         {
+            CellEditEndingCommand = new RelayCommand(CellEditEnding);
             _context.Authors.Include(a => a.Books).Load();
             AuthorList = _context.Authors.Local.ToObservableCollection();
+        }
 
+        private void CellEditEnding(object obj)
+        {
+            Debug.WriteLine($"Saved the Changes: {AuthorList}");
+            _context.SaveChanges();
         }
     }
 }
