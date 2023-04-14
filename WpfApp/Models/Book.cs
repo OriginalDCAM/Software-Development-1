7ba8d9cc33bbc064cb930de192d7fc8b33e7b7c8
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-14 21:56:33 +0200

    + Updated the id field so i can be nullable

--- WpfApp/Models/Book.cs
+++ WpfApp/Models/Book.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WpfApp.Models
 {
     public class Book
     {
-        public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? Id { get; set; }
         public string Name { get; set; }
 
         public string Description { get; set; }
