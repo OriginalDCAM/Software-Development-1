ad61d19b445abd3a97d198a1ea8a8341f6e6a5b9
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-14 22:19:31 +0200

    + Added more error handling

--- WpfApp/ViewModels/AuthorBooksViewModel.cs
+++ WpfApp/ViewModels/AuthorBooksViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -49,7 +51,30 @@ namespace WpfApp.ViewModels
 
         public ObservableCollection<Book> AuthorBooks =>
             SelectedAuthor != null ? new ObservableCollection<Book>(SelectedAuthor?.Books) : null;
+        
+        private string _errorContent;
 
+        public string ErrorContent
+        {
+            get => _errorContent;
+            set
+            {
+                _errorContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _successContent;
+
+        public string SuccessContent
+        {
+            get => _successContent;
+            set
+            {
+                _successContent = value;
+                OnPropertyChanged();
+            }
+        }
 
         public AuthorBooksViewModel()
         {
@@ -64,13 +89,18 @@ namespace WpfApp.ViewModels
 
         private void SaveChanges(object obj)
         {
+            ClearErrors("exception");
             try
             {
                 _context.SaveChanges();
+                SuccessContent = "Item Succesvol gewijzigd!";
             }
             catch (Exception e)
             {
                 Debug.WriteLine($"{e.Message}");
+                AddError("exception", $"Er is iets misgegaan probeer van page te veranderen!");
+                ErrorContent = GetErrors("exception")?.Cast<string>().FirstOrDefault() ?? "";
+                SuccessContent = "";
             }
         }
 
@@ -86,8 +116,46 @@ namespace WpfApp.ViewModels
             }
             catch (Exception e)
             {
-                Debug.WriteLine(e.Message);
+                Debug.WriteLine($"{e.Message}");
+                AddError(nameof(e), $"Er is iets misgegaan probeer van page te veranderen!");
+                ErrorContent = GetErrors(nameof(e))?.Cast<string>().FirstOrDefault() ?? "";
+                SuccessContent = "";
             }
         }
+        
+        // Code voor valideren van properties en error handling
+                private readonly Dictionary<string, List<string>>
+                    _errorsByPropertyName = new();
+        
+                public bool HasErrors => _errorsByPropertyName.Any();
+        
+                public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        
+                public IEnumerable GetErrors(string propertyName)
+                {
+                    return _errorsByPropertyName.TryGetValue(propertyName, out var value) ? value : null;
+                }
+        
+                private void OnErrorsChanged(string propertyName)
+                {
+                    ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+                }
+        
+                private void AddError(string propertyName, string error)
+                {
+                    if (!_errorsByPropertyName.ContainsKey(propertyName))
+                        _errorsByPropertyName[propertyName] = new List<string>();
+        
+                    if (_errorsByPropertyName[propertyName].Contains(error)) return;
+                    _errorsByPropertyName[propertyName].Add(error);
+                    OnErrorsChanged(propertyName);
+                }
+        
+                private void ClearErrors(string propertyName)
+                {
+                    if (!_errorsByPropertyName.ContainsKey(propertyName)) return;
+                    _errorsByPropertyName.Remove(propertyName);
+                    OnErrorsChanged(propertyName);
+                }
     }
 }
