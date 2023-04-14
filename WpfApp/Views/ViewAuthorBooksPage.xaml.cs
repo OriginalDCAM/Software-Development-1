83fd53cc3fb2e2fbacb357a9305daf8d957417db
Denzel // TrueStatement <denzelannette@hotmail.com>
2023-04-14 22:34:00 +0200

    + .

---
+++ WpfApp/Views/ViewAuthorBooksPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using WpfApp.ViewModels;
+
+namespace WpfApp.Views
+{
+    public partial class ViewAuthorBooksPage : Page
+    {
+        public ViewAuthorBooksPage()
+        {
+            InitializeComponent();
+            DataContext = new AuthorBooksViewModel();
+        }
+    }
+}
